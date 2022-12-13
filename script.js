da30fc4ad6a3ec9d78b7d5e2f06f8fbabcb4d357
Christopher Fuchs <fuchs-christopher@gmx.de>
2022-12-13 20:38:32 +0100

    Added endScreen

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let fields = [];
 let currentShape = 'cross';
+let gameOver = false;
 
 
 function fillShape(id) {
-    if (!fields[id]) {
+    if (!fields[id] && !gameOver) {
         if (currentShape == 'cross') {
             currentShape = 'circle';
             document.getElementById('player1').classList.add('playerInactive');
@@ -39,37 +40,49 @@ function checkForWin() {
     let winner;
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line1').style.transform = 'scale(1)';
     }
 
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
+        document.getElementById('line2').style.transform = 'scale(1)';
     }
 
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        document.getElementById('line3').style.transform = 'scale(1)';
     }
 
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line4').style.transform = 'rotate(90deg) scale(1)';
     }
 
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        document.getElementById('line5').style.transform = 'rotate(90deg) scale(1)';
     }
 
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line6').style.transform = 'rotate(90deg) scale(1)';
     }
 
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line7').style.transform = 'rotate(45deg) scale(1)';
     }
 
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line8').style.transform = 'rotate(-45deg) scale(1)';
     }
 
     if (winner) {
         console.log('WINNER', winner);
+        gameOver = true;
+        setTimeout(function () {
+            document.getElementById('gameOver').classList.remove('d-none');
+        }, 1000);
     }
 }
